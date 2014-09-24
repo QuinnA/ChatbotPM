e7833dc57600031478128acd36ae736b109a3c42
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2014-09-24 13:23:56 -0600

    Adding to repo

---
+++ src/chatbot/controller/ChatbotAppController.java
@@ -0,0 +1,33 @@
+package chatbot.controller;
+
+import javax.swing.JOptionPane;
+
+import chatbot.view.ChatbotView;
+
+public class ChatbotAppController
+{
+	private ChatbotView applicationView;
+	
+	public ChatbotAppController()
+	{
+		applicationView = new ChatbotView(this);
+	}
+	
+	public void start()
+	{
+		String result = applicationView.showChatbot("Cody");
+		
+		if (result.equalsIgnoreCase("exit"))
+		{
+			quit();
+		}
+	}
+	
+	private void quit()
+	{
+		JOptionPane.showMessageDialog(null, "good bye cruel world");
+		System.exit(0);
+		
+	}
+
+}
